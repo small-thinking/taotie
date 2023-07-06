907ed59c7fdac94cf4529d2859e9c5805b46c346
Yx Jiang <2237303+yxjiang@users.noreply.github.com>
2023-07-06 07:48:04 -0700

    Minor refactor

--- taotie/sources/github.py
+++ taotie/sources/github.py
@@ -29,66 +29,29 @@ class GithubTrends(BaseSource):
         pass
 
     async def run(self):
+        """
+        This method runs the GithubTrends source.
+
+        It does the following:
+
+        - Gets trending repositories from Github using get_trending_repos()
+        - Processes each repository using process_repo() to extract information and generate a Github event
+        - Sends the Github event to the sink using _send_data()
+        - Sleeps for 10 seconds between processing repositories
+        - Logs that the Github event was checked and will check again in self.check_interval seconds
+        - Sleeps for self.check_interval seconds before repeating
+
+        Args:
+            session (aiohttp.ClientSession): The aiohttp ClientSession used to make requests
+
+        Returns:
+            None
+        """
         async with aiohttp.ClientSession() as session:
             while True:
-                async with session.get(self.url, verify_ssl=False) as response:
-                    soup = BeautifulSoup(await response.text(), "html.parser")
-
-                repo_blob = soup.find_all("article", {"class": "Box-row"})
-                for idx, blob in enumerate(repo_blob):
-                    repo_name = blob.find("h2", {"class": "h3 lh-condensed"}).a["href"]
-                    repo_url = (
-                        "https://github.com"
-                        + blob.find("h2", {"class": "h3 lh-condensed"}).a["href"]
-                    )
-                    repo_desc_blob = blob.find(
-                        "p", {"class": "col-9 color-fg-muted my-1 pr-4"}
-                    )
-                    repo_desc = repo_desc_blob.text.strip() if repo_desc_blob else ""
-                    repo_lang_blob = blob.find(
-                        "span", {"class": "d-inline-block ml-0 mr-3"}
-                    )
-                    repo_lang = repo_lang_blob.text.strip() if repo_lang_blob else ""
-                    star_and_fork = blob.find_all(
-                        "a", {"class": "Link--muted d-inline-block mr-3"}
-                    )
-                    repo_star = star_and_fork[0].text.strip()
-                    repo_fork = star_and_fork[1].text.strip()
-                    # Extract the detailed description from the github main README.md if any.
-                    readme_url = (
-                        f"https://raw.githubusercontent.com{repo_name}/master/README.md"
-                    )
-                    if not check_url_exists(readme_url):
-                        readme_url = f"https://raw.githubusercontent.com{repo_name}/main/README.md"
-                    repo_readme = ""
-                    try:
-                        async with session.get(
-                            readme_url, verify_ssl=False
-                        ) as readme_response:
-                            if readme_response.status == 200:
-                                repo_readme = await readme_response.text()
-                                repo_readme = repo_readme[: self.readme_truncate_size]
-                            else:
-                                self.logger.warning(
-                                    f"Failed to fetch from {readme_url}. Status: {readme_response.status}"
-                                )
-                                raise Exception(readme_response.status)
-                    except Exception as e:
-                        self.logger.warning(
-                            f"Failed to fetch from {readme_url}. Reason: {e}"
-                        )
-
-                    github_event = Information(
-                        type="github-repo",
-                        datetime_str=get_datetime(),
-                        id=repo_name,
-                        uri=repo_url,
-                        content=repo_readme,
-                        repo_desc=repo_desc,
-                        repo_lang=repo_lang,
-                        repo_star=repo_star,
-                        repo_fork=repo_fork,
-                    )
+                repos = await self.get_trending_repos(session)
+                for repo in repos:
+                    github_event = await self.process_repo(session, repo)
                     res = await self._send_data(github_event)
                     if res:
                         self.logger.debug(f"{idx}: {github_event.encode()}")
@@ -97,3 +60,68 @@ class GithubTrends(BaseSource):
                     f"Github event checked. Will check again in {self.check_interval} seconds."
                 )
                 await asyncio.sleep(self.check_interval)
+
+    async def get_trending_repos(self, session):
+        """
+        Gets trending repositories from Github.
+
+        Args:
+            session (aiohttp.ClientSession): The aiohttp ClientSession used to make requests
+
+        Returns:
+            repo_blob (list): A list of BeautifulSoup elements representing trending repositories.
+        """
+
+        async with session.get(self.url, verify_ssl=False) as response:
+            soup = BeautifulSoup(await response.text(), "html.parser")
+        repo_blob = soup.find_all("article", {"class": "Box-row"})
+        return repo_blob
+
+    async def process_repo(self, session, repo_blob):
+        """
+        Gets information from a Github repository and returns a Github event.
+
+        Args:
+            session (aiohttp.ClientSession): The aiohttp ClientSession used to make requests
+            repo_blob (BeautifulSoup element): A BeautifulSoup element representing a trending Github repository
+
+        Returns:
+            github_event (Information): An Information object representing the Github event
+        """
+
+        repo_name = repo_blob.find("h2", {"class": "h3 lh-condensed"}).a["href"]
+        # Extract other repo info...
+        readme = await self.get_readme(session, repo_name)
+        github_event = Information(
+            type="github-repo",
+            datetime_str=get_datetime(),
+            id=repo_name,
+            uri=repo_url,
+            content=readme,
+            # Other fields...
+        )
+        return github_event
+
+    async def get_readme(self, session, repo_name):
+        """
+        Fetches the README.md file from a Github repository.
+
+        Args:
+            session (aiohttp.ClientSession): The aiohttp ClientSession used to make requests
+            repo_name (str): The name of the Github repository
+
+        Returns:
+            readme (str): The content of the README.md file
+
+        Raises:
+            Exception: If the request fails with a status other than 200.
+        """
+        readme_url = f"https://raw.githubusercontent.com{repo_name}/master/README.md"
+        async with session.get(readme_url, verify_ssl=False) as response:
+            if response.status == 200:
+                return await response.text()
+            else:
+                self.logger.warning(
+                    f"Failed to fetch from {readme_url}. Status: {response.status}"
+                )
+                raise Exception(response.status)
