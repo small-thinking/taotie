59f21a5386909b5c47621ba75374f06111a8c5a3
Yx Jiang <2237303+yxjiang@users.noreply.github.com>
2023-11-06 21:43:18 -0800

    Model update

--- taotie/reporter/notion_reporter.py
+++ taotie/reporter/notion_reporter.py
@@ -42,7 +42,7 @@ class NotionReporter(BaseReporter):
         if not os.getenv("OPENAI_API_KEY"):
             raise ValueError("Please set OPENAI_API_KEY in .env.")
         openai.api_key = os.getenv("OPENAI_API_KEY")
-        self.model_type = kwargs.get("model_type", "gpt-3.5-turbo-16k")
+        self.model_type = kwargs.get("model_type", "gpt-3.5-turbo-1106")
         # Prompt.
         language = kwargs.get("language", "English")
         if "github-repo" in self.topic_filters:
@@ -238,7 +238,7 @@ class NotionReporter(BaseReporter):
         '''
         """
         # Truncate.
-        truncate_size = 12000 if self.model_type == "gpt-3.5-turbo-16k" else 7000
+        truncate_size = 12000 if self.model_type == "gpt-3.5-turbo-1106" else 7000
         content_prompt = content_prompt[:truncate_size]
         self.logger.output(f"Content prompt: {content_prompt}")
         # Rough estimation of remaining tokens for generation.
